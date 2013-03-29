11b607b1fe46fd1196ff58c5798352c6f4b6f143
H Liang <hliang@ksu>
2013-03-28 21:55:30 -0500

    successful abstract.java; rm class files

---
+++ abstract2.java
@@ -0,0 +1,45 @@
+abstract class Shape{
+   final int b = 20;
+   public void display(){
+     System.out.println("This is display method");
+   }
+   abstract public void calculateArea(); 
+}
+ 
+abstract class Rectangle extends Shape{
+}
+
+class Rectan {
+   public static void main(String args[]){
+      Rectangle obj = new Rectangle();
+      obj.display();
+     //obj.b=200;
+  }
+}
+
+
+/*
+abstract class Shape
+{
+       void display()
+       {
+       }   
+}
+
+class Rectangle extends Shape
+{
+       void display()
+       {
+              System.out.println("You are using rectangle class");
+       }
+}
+
+class AbstractClassDemo
+{
+       public static void main(String args[])
+       {
+               Shape sObj = new Rectangle();
+               sObj.display();
+      }
+}
+*/
